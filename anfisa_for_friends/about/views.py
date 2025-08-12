3c3b77bbff04d3226f90111563f8d6d9f0a24659
E.N.Sklyarov <e.sklyarov@tns-e.ru>
2025-08-12 14:47:36 +0300

    done

--- anfisa_for_friends/about/views.py
+++ anfisa_for_friends/about/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from contest.models import Contest
 
 
-def description(request):
-    template = 'about/description.html'
-    return render(request, template)
+class Description(TemplateView):
+    template_name = 'about/description.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["counts"] = Contest.objects.count()
+        return context
+    
